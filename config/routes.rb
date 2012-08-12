2d1e445f248758b73be5372c1c550377941eb802
Ankur Oberoi <aoberoi@gmail.com>
2012-08-12 18:52:58 -0400

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  # get "users/new"
 
-  get "static_pages/help"
+  root to: "static_pages#home"
 
-  get "static_pages/about"
-
-  get "static_pages/contact"
+  get '/help', to: "static_pages#help"
+  get '/about', to: "static_pages#about"
+  get '/contact', to: "static_pages#contact"
+  get '/signup', to: "users#new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
